1c7bb59a20121c685fb17e6e82b634e1fe5b4f46
EC2 Default User <ec2-user@ip-172-31-19-140.eu-north-1.compute.internal>
2025-01-09 08:15:34 +0000

    [ToDo] Aboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  post 'books' => 'books#create'
-  resources :users, only: [:show, :edit]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  root to: "homes#top"
   devise_for :users
+  get 'homes/about' => 'homes#about', as: 'about'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
+  root to: "homes#top"
 end
